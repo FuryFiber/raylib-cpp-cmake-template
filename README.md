235772c6de30e7d31257a2e07b1cd30434305bec
FuryFiber <flor.mertens@proximus.be>
2023-04-18 21:41:25 +0200

    init

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-# raylib-c++cmake-template
-Very basic setup for raylib with c++ and raylib
-cmakelist will download the raylib library from github if needed and set everything up.
-By running main.cpp a blank white window will be created.
+# raylib-cpp CMake Example Project
 
-NOTE: this is only setup to be used on linux
+Use this template to build a [raylib-cpp](https://github.com/RobLoach/raylib-cpp) project using CMake.
+
+## Build
+
+```
+mkdir build
+cd build
+cmake ..
+make
+```
+
+### Web
+
+```
+mkdir build
+cd build
+emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-s USE_GLFW=3"
+emmake make
+```
+
+## Run
+
+```
+./raylib-cpp-example
+```
---
+++ main.cpp
@@ -0,0 +1,55 @@
+/*******************************************************************************************
+*
+*   raylib-cpp [core] example - Basic window (adapted for HTML5 platform)
+*
+*   This example is prepared to compile for PLATFORM_WEB, PLATFORM_DESKTOP and PLATFORM_RPI
+*   As you will notice, code structure is slightly diferent to the other examples...
+*   To compile it for PLATFORM_WEB just uncomment #define PLATFORM_WEB at beginning
+*
+*   This example has been created using raylib-cpp (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2015 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib-cpp.hpp"
+
+//----------------------------------------------------------------------------------
+// Global Variables Definition
+//----------------------------------------------------------------------------------
+int screenWidth = 800;
+int screenHeight = 450;
+
+//----------------------------------------------------------------------------------
+// Main Enry Point
+//----------------------------------------------------------------------------------
+int main()
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    raylib::Window window(screenWidth, screenHeight, "raylib-cpp [core] example - basic window");
+
+    SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!window.ShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    return 0;
+}
